7117ae08b7d5d9eec315c26312ec56fa9e589b34
winterestingwithyour <yudistiraadam3@gmail.com>
2026-05-31 10:37:19 +0700

    fix error syntax ini vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
-export default defineConfig(
+export default defineConfig({
   plugins: [
     vue(),
     vuetify({
       autoImport: true
     }),
   ],
+ }
 )
